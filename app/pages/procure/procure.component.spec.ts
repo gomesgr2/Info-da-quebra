0017cefc95c370f9f07ee0b848c2ad130f99048e
Gabriel Gomes de Oliveira Costa <70021084+gomesgr2@users.noreply.github.com>
2020-11-13 17:31:55 -0300

    Add files via upload

---
+++ app/pages/procure/procure.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProcureComponent } from './procure.component';
+
+describe('ProcureComponent', () => {
+  let component: ProcureComponent;
+  let fixture: ComponentFixture<ProcureComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProcureComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProcureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ app/pages/procure/procure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-procure',
+  templateUrl: './procure.component.html',
+  styleUrls: ['./procure.component.css']
+})
+export class ProcureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

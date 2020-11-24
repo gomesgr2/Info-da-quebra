dd4bf6d37f550dc6569fc2bf5c43cd0dfff3c791
Gabriel Gomes de Oliveira Costa <70021084+gomesgr2@users.noreply.github.com>
2020-11-24 09:58:53 -0300

    Add files via upload

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { ProcureComponent } from './pages/procure/procure.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [{path: '', component: HomeComponent }, { path: 'Sobre', component: SobreComponent }];
+const routes: Routes = [{path: '', component: HomeComponent },{ path: 'Home', component: HomeComponent},
+ { path: 'Sobre', component: SobreComponent }, { path : 'Procure', component: ProcureComponent}, {path : 'Noticias', component : NoticiasComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/app.module.ts
+++ app/app.module.ts
@@ -8,25 +8,53 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {IvyCarouselModule} from 'angular-responsive-carousel';
-import {CardModule} from 'primeng/card';
-
+import { ProcureComponent } from './pages/procure/procure.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import{MatIconModule} from '@angular/material/icon';
+import{MatButtonModule} from '@angular/material/button';
+import{MatTabsModule} from '@angular/material/tabs'
+import{MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core'
+import {MatSelectModule} from '@angular/material/select'
+import {MatInputModule} from '@angular/material/input'
+import {MatExpansionModule} from '@angular/material/expansion';
+import { FormComponent } from './view/form/form.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { AppService } from './service/app.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SobreComponent,
-    HomeComponent
+    HomeComponent,
+    ProcureComponent,
+    NoticiasComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ClarityModule,
-    BrowserAnimationsModule,
-    NgbModule,IvyCarouselModule, CardModule
+    BrowserAnimationsModule, NgbModule,MatCardModule,MatToolbarModule,
+    MatIconModule,
+   MatButtonModule,
+    MatTabsModule,
+   MatDialogModule,
+    MatFormFieldModule,
+   FormsModule, ReactiveFormsModule,
+    MatSelectModule,
+   MatInputModule,MatExpansionModule,FlexLayoutModule,HttpClientModule
+    
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AppService, {provide :MatDialogRef, useValue : {}}, FormComponent],
+  bootstrap: [AppComponent], schemas : [CUSTOM_ELEMENTS_SCHEMA],
+  entryComponents: []
 })
 export class AppModule { }

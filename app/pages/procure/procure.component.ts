f9470055829b1e279f1dc4345949bdd6939661d4
Gabriel Gomes de Oliveira Costa <70021084+gomesgr2@users.noreply.github.com>
2020-11-24 09:59:50 -0300

    Add files via upload

--- app/pages/procure/procure.component.ts
+++ app/pages/procure/procure.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Procure } from 'src/app/models/procure';
+import { AppService } from 'src/app/service/app.service';
 
 @Component({
   selector: 'app-procure',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./procure.component.css']
 })
 export class ProcureComponent implements OnInit {
+  localizacao = {
+    CEP : ''
+  } as unknown as Procure;
+  show = true;
+  json = false;
 
-  constructor() { }
+  procure: Procure[];
+  json_procure: any[];
+
+
+  constructor(public appService: AppService, 
+    public sanitizer: DomSanitizer
+    ) {}
 
   ngOnInit(): void {
-  }
+    this.postProcure();}
+    postProcure() {
+      this.appService.PostProcure(this.localizacao).subscribe((dados_procure) => {
+        let procure = dados_procure;
+        this.json_procure = procure.Result;})
+        this.json_procure.forEach((locais) => {
+          locais.link = this.sanitizer.bypassSecurityTrustResourceUrl(
+            locais.rota   );
+          });
+        }
 
-}
+
+      }
+
+
